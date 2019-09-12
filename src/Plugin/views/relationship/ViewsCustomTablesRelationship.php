527eb5e9a35a3685eef8b2668dce10500ba5f77d
Diao Diallo <diao_diallo@gn.jsi.com>
2019-09-12 17:45:05 +0000

    Adding relationship

---
+++ src/Plugin/views/relationship/ViewsCustomTablesRelationship.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ddiallo
+ * Date: 9/10/19
+ * Time: 2:14 PM
+ */
+namespace Drupal\custom_tables\Plugin\views\relationship;
+
+use Drupal\views\Plugin\views\relationship\RelationshipPluginBase;
+
+/**
+ * Default implementation of the base relationship plugin.
+ *
+ * @ingroup views_relationship_handlers
+ *
+ * @ViewsRelationship("views_custom_tables_relationship")
+ */
+class ViewsCustomTablesRelationship extends RelationshipPluginBase {
+
+}
